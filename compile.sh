925914a6bc75a42bc35b6473e91d2eca56f2278e
Nearkitai <45140946+Nearkitai@users.noreply.github.com>
2018-12-15 20:09:09 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -2,12 +2,11 @@
 
 # Let's compile Nearkitai daemon
 
-
 chmod 755 src/leveldb/build_detect_platform
 chmod 755 src/secp256k1/autogen.sh
 cd src/leveldb
 make libleveldb.a libmemenv.a
-mkdir -p obj/support
 cd ..
+mkdir -p obj/support
 make -f makefile.unix
 strip nearkitaid
