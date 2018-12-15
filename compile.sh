0458d09bc8c73374117143198a2f52c784bdbc18
Nearkitai <45140946+Nearkitai@users.noreply.github.com>
2018-12-15 19:51:47 +0800

    Add files via upload

--- compile.sh
+++ compile.sh
@@ -7,6 +7,7 @@ chmod 755 src/leveldb/build_detect_platform
 chmod 755 src/secp256k1/autogen.sh
 cd src/leveldb
 make libleveldb.a libmemenv.a
+mkdir -p obj/support
 cd ..
 make -f makefile.unix
 strip nearkitaid
